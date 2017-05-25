115f4499eb9052f5c8c94113396eef92cf7c4518
ManuLEM <manuel.lemaire@gmail.com>
2017-05-25 18:09:36 +0200

    Add heroku support to listen right port

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
+import os
 from eve import Eve
 from flask import jsonify
+
+# Heroku support: bind to PORT if defined, otherwise default to 5000.
+if 'PORT' in os.environ:
+    port = int(os.environ.get('PORT'))
+    # use '0.0.0.0' to ensure your REST API is reachable from all your
+    # network (and not only your computer).
+    host = '0.0.0.0'
+else:
+    host = '127.0.0.1'
+    port = 5000
+
 app = Eve()
+
 
 @app.route("/client_list")
 def clients():
@@ -16,4 +29,4 @@ def clients():
 	)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	app.run(host=host,port=port)
