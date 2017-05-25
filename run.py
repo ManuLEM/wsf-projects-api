ad840b39371871c3d123661a832d815c4ef0c5dc
ManuLEM <manuel.lemaire@gmail.com>
2017-05-25 17:47:53 +0200

    Add some fixes for heroku

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 from eve import Eve
 app = Eve()
 
+@app.route("/client_list")
+def clients():
+	return jsonify(
+		{
+			'text': 'Quel est le client de ton projet?',
+			'quick_replies': [{
+				'content_type': 'text',
+				'title': 'A',
+				'payload': '|'
+			}]
+		}
+	)
+
 if __name__ == '__main__':
     app.run()
